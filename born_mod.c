15358eb9375ba625220ba9e9eb580fb6fdc810f2
wencaixu007 <41809130+wencaixu007@users.noreply.github.com>
2018-07-28 10:42:00 +0800

    for commit

--- born_mod.c
+++ born_mod.c
@@ -11,32 +11,8 @@ void born_modeling(char FN1[],float **vp,float **ref,float **rho,int nx,int nz,
 	FILE *fp;
 	//fp=fopen(FN1,"wb");	
 	nxx=nx+2*npd;nzz=nz+2*npd;
-	//################
-	coeff_x1=alloc1d(nxx);zero1d(coeff_x1,nxx);
-	coeff_z1=alloc1d(nzz);zero1d(coeff_z1,nzz);
-	coeff_x2=alloc1d(nxx);zero1d(coeff_x2,nxx);
-	coeff_z2=alloc1d(nzz);zero1d(coeff_z2,nzz);
-	pml_init(coeff_x1,coeff_x2,coeff_z1,coeff_z2,nx,nz,npd,dt,d0);
-	//################
-	c=alloc2d(mm+1,mm+1);zero2d(c,mm+1,mm+1);	
-	cal_c(mm,c);
-	//################
-	u=alloc2d(nxx,nzz);zero2d(u,nxx,nzz);
-	w=alloc2d(nxx,nzz);zero2d(w,nxx,nzz);
-	p=alloc2d(nxx,nzz);zero2d(p,nxx,nzz);
-	px=alloc2d(nxx,nzz);zero2d(px,nxx,nzz);
-	pz=alloc2d(nxx,nzz);zero2d(pz,nxx,nzz);
-	u0=alloc2d(nxx,nzz);zero2d(u0,nxx,nzz);
-	w0=alloc2d(nxx,nzz);zero2d(w0,nxx,nzz);
-	p0=alloc2d(nxx,nzz);zero2d(p0,nxx,nzz);
-	px0=alloc2d(nxx,nzz);zero2d(px0,nxx,nzz);
-	pz0=alloc2d(nxx,nzz);zero2d(pz0,nxx,nzz);	
-	//################
-	s=alloc1d(nt);zero1d(s,nt);
-	source(s,nt,fmain,dt,pfac);
-	//################
-	for(it=0;it<nt;it++)
-	{
+	/
+
 		//if((it+1)%500==0)printf("it=%d\n",it+1);
 		update_p(u0,w0,p0,px0,pz0,vp,c,nxx,nzz,mm,dx,dz,dt,
 	             coeff_x1,coeff_x2,coeff_z1,coeff_z2,1);
